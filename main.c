8d10eb0a09d13d7154c22a72d1009f40f0eb183b
Arinjay Singhal <137890548+ArinjaySinghal19@users.noreply.github.com>
2025-01-29 15:44:57 +0530

    Update main.c

--- main.c
+++ main.c
@@ -21,31 +21,31 @@ int main(){
     cell **sheet;
     initialize_sheet(&sheet, rows, cols);
 
-    // while(1){
-    //     double start = clock();
+    while(1){
+        double start = clock();
 
-    //     char *input = (char *)malloc(100 * sizeof(char));
-    //     fgets(input, 100, stdin);
+        char *input = (char *)malloc(100 * sizeof(char));
+        fgets(input, 100, stdin);
 
-    //     if(invalid_input(input)){
-    //         display_sheet(sheet, rows, cols);
-    //         double end = clock();
-    //         double time = (end - start) / CLOCKS_PER_SEC;
-    //         printf("[%.2f s] (Invalid Input) > ", time);
-    //         continue;
-    //     }
+        if(invalid_input(input)){
+            display_sheet(sheet, rows, cols);
+            double end = clock();
+            double time = (end - start) / CLOCKS_PER_SEC;
+            printf("[%.2f s] (Invalid Input) > ", time);
+            continue;
+        }
 
-    //     process_input(&input, &sheet, rows, cols);
+        process_input(&input, &sheet, rows, cols);
 
-    //     display_sheet(sheet, rows, cols);
+        display_sheet(sheet, rows, cols);
 
-    //     double end = clock();
+        double end = clock();
 
-    //     double time = (end - start) / CLOCKS_PER_SEC;
+        double time = (end - start) / CLOCKS_PER_SEC;
 
-    //     printf("[%.2f s] (ok) > ", time);
+        printf("[%.2f s] (ok) > ", time);
 
-    // }
+    }
 
     return 0;
 }
