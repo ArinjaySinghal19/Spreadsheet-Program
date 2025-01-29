d40fb51dfef2de66400d4f5e6b635aaedccca38c
Arinjay Singhal <137890548+ArinjaySinghal19@users.noreply.github.com>
2025-01-29 15:02:07 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <time.h>
+
+typedef struct cell{
+    int value;
+    int row;
+    int col;
+} cell;
+
+
+int main(){
+
+    int rows, cols;
+    scanf("%d %d", &rows, &cols);
+    getchar();
+
+    if(!valid_input(rows, cols)){
+        printf("Invalid input\n");
+        return 0;
+    }
+
+    int **sheet;
+    initialize_sheet(&sheet, rows, cols);
+
+    while(1){
+        double start = clock();
+
+        char *input = (char *)malloc(100 * sizeof(char));
+        fgets(input, 100, stdin);
+
+        if(invalid_input(input)){
+            display_sheet(sheet, rows, cols);
+            double end = clock();
+            double time = (end - start) / CLOCKS_PER_SEC;
+            printf("[%.2f s] (Invalid Input) > ", time);
+            continue;
+        }
+
+        process_input(&input, &sheet, rows, cols);
+
+        display_sheet(sheet, rows, cols);
+
+        double end = clock();
+
+        double time = (end - start) / CLOCKS_PER_SEC;
+
+        printf("[%.2f s] (ok) > ", time);
+
+    }
+}
