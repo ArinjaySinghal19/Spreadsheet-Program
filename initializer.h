ff98e951e85e68df9378db1c0dc674f772e4c730
Arinjay Singhal <137890548+ArinjaySinghal19@users.noreply.github.com>
2025-02-05 15:37:04 +0530

    New structures added
    Basic Memory functions and structures updated

--- initializer.h
+++ initializer.h
@@ -1,12 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>    
+#include <stdbool.h>   
+
+typedef struct Node{
+    int row;
+    int col;
+    struct Node *next;
+} Node;
+
+typedef struct {
+    int target[2]; // Target cell (row, col)
+    int expression_type; // 0 for value, 1 for expression, 2 for function
+    int is_sleep; // 1 if SLEEP function, 0 otherwise
+    int sleep_value[2]; // Sleep value (if is_sleep=1)
+    int value[2]; // Value (if expression_type=0)
+    int expression_cell_1[2]; // First cell in expression
+    int expression_cell_2[2]; // Second cell in expression
+    char expression_operator; // Operator in expression ( +, -, *, /)
+    char function_operator; 
+    int function_range[4]; // Function range (start row, start col, end row, end col)
+} ParsedInput;
 
 typedef struct cell{
     int value;
     int row;
     int col;
+    Node *dependencies;
+    int num_dependencies;
+    Node *depends_on;
+    bool is_dirty;
+    int dirty_parents;
+    ParsedInput parsed;
 } cell;
+
+
 
 bool valid_input(int rows, int cols){
     if(rows < 1 || rows > 999 || cols < 1 || cols > 18278){
@@ -43,4 +70,24 @@ void initialize_sheet(cell ***sheet, int rows, int cols){
             (*sheet)[i][j].col = j;
         }
     }
+}
+
+
+void free_dependencies(Node *dependencies){
+    Node *temp = dependencies;
+    while(temp != NULL){
+        Node *next = temp->next;
+        free(temp);
+        temp = next;
+    }
+}
+
+void free_sheet(cell **sheet, int row, int col){
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            free_dependencies(sheet[i][j].dependencies);
+        }
+        free(sheet[i]);
+    }
+    free(sheet);
 }
